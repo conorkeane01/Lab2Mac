fbd2d84b07c539b8dede9a308c19fdb3d75ebbc4
conorkeane <g00391846@atu.ie>
2023-10-05 15:45:07 +0100

    Created Lab2 project

---
+++ src/main/java/ie/atu/lab2mac/lab2mac/Lab2MacApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2mac.lab2mac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2MacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2MacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab2mac/lab2mac/Lab2MacApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2mac.lab2mac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2MacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
