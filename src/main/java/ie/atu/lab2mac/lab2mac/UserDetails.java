d22b296bd8210e59cf965e1aa3d354d24a1014e4
conorkeane <g00391846@atu.ie>
2023-10-09 20:46:36 +0100

    Added name and email to UserController and created UserDetails

--- src/main/java/ie/atu/lab2mac/lab2mac/UserDetails.java
+++ src/main/java/ie/atu/lab2mac/lab2mac/UserDetails.java
@@ -1,4 +1,13 @@
 package ie.atu.lab2mac.lab2mac;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetails {
+    private String name;
+    private String email;
 }
